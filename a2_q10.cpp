e1f07023f4fcf14d0551641e4fd503daf6561c70
Tanvi-webdev <66518298+Tanvi-webdev@users.noreply.github.com>
2023-01-25 11:34:16 +0530

    submitting ques_10 of assignment_2

---
+++ a2_q10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int r=1; r<=n; r++)
+    {
+        int mr=(n+1)/2;
+        for(int c=1; c<=n; c++)
+        {
+            if(r<=mr && c==mr+1-r || c==mr-1+r)
+                cout<<"*\t";
+            else if(r>mr && c==r-mr+1  || c==n-(r-mr))
+                cout<<("*\t");
+            else
+                cout<<"\t";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
